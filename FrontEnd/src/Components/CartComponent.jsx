8b8cf8c4d829824b4f3f395dd995d58191073403
yaroslav8765 <pechorkin2014@gmail.com>
2025-05-30 12:59:53 +0300

    feat: enhance CartComponent and SingleCartItem with delete functionality, improved quantity handling, and error management

--- FrontEnd/src/Components/CartComponent.jsx
+++ FrontEnd/src/Components/CartComponent.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useRouteLoaderData, useNavigate  } from "react-router-dom";
+import { useRouteLoaderData, useNavigate, Link  } from "react-router-dom";
 import { getAuthToken, checkAuthLoader, removeToken } from "../../util/auth.js"
 import SingleCartItem from "../ui/SingleCartItem.jsx";
 import CrititcalErrorWindow from "../ui/CrititcalErrorWindow.jsx";
@@ -38,14 +38,19 @@ function CartComponent(){
     getUsersGoods();
     },[])
 
-    return <>
+    function handleDelete(deletedId) {
+    setUsersGoods((prevGoods) => prevGoods.filter((item) => item.goods.id !== deletedId));
+    }
+
+
+    return <div className="flex flex-col w-full">
         {isError && <CrititcalErrorWindow message={errorMessage}/>}
-        <div className=" w-full h-[609px] shadow-md rounded-2xl">
+        <div className=" w-full h-[608px] shadow-md rounded-2xl">
               <h2 className="text-gray-800 text-3xl font-semibold text-center">Your Cart</h2>
                 {usersGoods.length === 0 ? (
                 <p className="text-gray-800 text-3xl font-semibold text-center">Your cart is empty</p>
                 ) : (
-                <div className=" max-h-[60vh] overflow-y-auto rounded-xl shadow-lg z-50 space-y- scroll-smooth">
+                <div className=" max-h-[572px] overflow-y-auto rounded-xl shadow-lg z-50 space-y- scroll-smooth">
                     <div className="flex flex-col w-full">
                     {usersGoods.map((item, index) => (
                         <SingleCartItem
@@ -58,6 +63,7 @@ function CartComponent(){
                         id={item.goods.id}
                         description={item.goods.description}
                         quantity={item.quantity}
+                        onDelete={handleDelete}
                         />
 
                     ))}
@@ -65,7 +71,8 @@ function CartComponent(){
                 </div>
                 )}
         </div>
-    </>
+        <Link to="/check-out" className="flex items-center justify-center rounded-3xl gradient-btn-green my-4 mx-40 h-[50px]">Proceed to checkout</Link>
+    </div>
 }
 
 export default CartComponent;
--- FrontEnd/src/ui/SingleCartItem.jsx
+++ FrontEnd/src/ui/SingleCartItem.jsx
@@ -1,20 +1,108 @@
 import SingleSearchBarResult from "./SingleSearchBarResult";
 import { useState } from "react";
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import { checkAuthLoader, getAuthToken } from "../../util/auth";
 
 function SingleCartItem(props) {
+  const API_URL = import.meta.env.VITE_API_URL;
   const [quantity, setQuantity] = useState(props.quantity || 1);
 
-  function plusGoodHandled(){
-
+  async function plusGoodHandled(){
+    const authResult = checkAuthLoader();
+    if(authResult) return authResult;
+    const token = getAuthToken();
+    const response = await fetch(`${API_URL}/user/basket-edit`,{
+      method:"PUT",
+      headers:{
+        "Content-type":"application/json",
+        "Authorization":`Bearer ${token}`
+      },
+    body: JSON.stringify({
+      goods_id: props.id,
+      new_quantity: quantity + 1
+    })
+    });
+    if(response.ok){
+    setQuantity(quantity+1);
+    }
   }
 
-  function minusGoodHandled(){
-    
+  async function minusGoodHandled(){
+    const authResult = checkAuthLoader();
+    if(authResult) return authResult;
+    const token = getAuthToken();
+    const response = await fetch(`${API_URL}/user/basket-edit`,{
+      method:"PUT",
+      headers:{
+        "Content-type":"application/json",
+        "Authorization":`Bearer ${token}`
+      },
+    body: JSON.stringify({
+      goods_id: props.id,
+      new_quantity: quantity - 1
+    })
+    });
+    if(response.ok){
+      setQuantity(quantity-1);
+    }
   }
+
+async function changeGoodHandler(e) {
+  const authResult = checkAuthLoader();
+  if(authResult) return authResult;
+  const token = getAuthToken();
+
+  const newValue = Number(e.target.value);
+
+  const response = await fetch(`${API_URL}/user/basket-edit`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify({
+      goods_id: props.id,
+      new_quantity: newValue
+    })
+  });
+
+  console.log("Response status:", response.status, response.statusText);
+
+  if (response.ok) {
+    setQuantity(newValue);
+  } else {
+    console.error("Ошибка:", response.status);
+    const errorData = await response.json();
+    console.error("Детали:", errorData);
+  }
+}
   
+
+
+
+async function deleteGoodHandler() {
+  const authResult = checkAuthLoader();
+  if(authResult) return authResult;
+  const token = getAuthToken();
+
+  const response = await fetch(`${API_URL}/user/basket-delete-good?goods_id=${props.id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+      "Authorization": `Bearer ${token}`
+    }
+  });
+
+    if (response.ok) {
+      props.onDelete(props.id);
+    } else {
+      console.error("Delete error:", response.status);
+    }
+}
+
+
   return (
-    <div className="flex">
+    <div className="flex justify-around px-6">
         <div className="px-2 w-[600px] box-border">
         <SingleSearchBarResult
             img={props.image_url}
@@ -31,19 +119,19 @@ function SingleCartItem(props) {
           <div className="flex gap-2">
             <button 
             className="bg-gray-400 mt-2 rounded-md w-[40px] h-[40px] gradient-btn-red" 
-            onClick={plusGoodHandled}>
+            onClick={minusGoodHandled}>
             -
             </button>
             <input 
-            className="border-1 border-gray-500  mt-2 rounded-md w-[40px] h-[40px]
+            className="border-1 border-gray-500  mt-2 rounded-md w-[60px] h-[40px]
             focus:border-cyan-600 focus:ring-1 focus:ring-cyan-300 outline-none text-gray-600 text-center
             "
             value={quantity}
-            onChange={(e) => setQuantity(e.target.value)}
+            onChange={changeGoodHandler}
             ></input>
             <button 
             className="bg-gray-400 mt-2 rounded-md w-[40px] h-[40px] gradient-btn-green"
-            onClick={minusGoodHandled}>
+            onClick={plusGoodHandled}>
             +
             </button>
           </div>
@@ -51,7 +139,11 @@ function SingleCartItem(props) {
             <p className="text-gray-600 text-xl">Total:</p>
             <p className="text-gray-900 text-xl font-bold">{quantity * props.price}</p>
           </div>
-          <button className="bg-gray-50 text-gray-600 rounded-md h-[30px] hover:bg-gray-300 transition-bg duration-200"><DeleteOutlineIcon/> Delete </button>
+          <button 
+            className="bg-gray-50 text-gray-600 rounded-md h-[30px] hover:bg-gray-300 transition-bg duration-200"
+            onClick={deleteGoodHandler}>
+              <DeleteOutlineIcon/> Delete 
+          </button>
         </div>
     </div>
   );
