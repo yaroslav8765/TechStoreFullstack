db44194c66b9ef6fc7c642a4662a9443a5ab856f
yaroslav8765 <pechorkin2014@gmail.com>
2025-06-22 12:17:33 +0300

    feat: enhance CreateReview component layout with padding; update Profile component for improved user data loading and error handling

--- frontend/src/components/CreateReview.jsx
+++ frontend/src/components/CreateReview.jsx
@@ -3,8 +3,12 @@
 
 function CreateReview(){
 
-    return<div className="flex flex-col w-full mx-auto shadow-md hover:shadow-lg border-1 border-gray-200 rounded-2xl h-[400px]">
-    
+    return<div className="flex flex-col w-full mx-auto shadow-md hover:shadow-lg border-1 border-gray-200 rounded-2xl h-[400px] p-4">
+        <h2 className="text-gray-800 text-2xl font-bold border-black mb-4 border-b pb-2">Write a customer review</h2>
+        
+
+
+
     </div>
 }
 
--- frontend/src/routers/Profile.jsx
+++ frontend/src/routers/Profile.jsx
@@ -11,138 +11,118 @@ import LoadingAnimation from "../components/LoadingAnimation.jsx";
 import { useLocation } from "react-router-dom";
 
 function Profile(){
-    const navigate = useNavigate();
-    const revalidator = useRevalidator();
-    const location = useLocation();
-    const [userInfo, setUserInfo] = useState([]);
-    const [currentMode, setCurretMode ]= useState("users-info");
-    
-    const [isLoading, setIsLoading] = useState(true);
-    const [isError, setIsError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+  const revalidator = useRevalidator();
+  const location = useLocation();
+  const [userInfo, setUserInfo] = useState([]);
+  const [currentMode, setCurretMode ]= useState("users-info");
+  
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-useEffect(() => {
-  const path = location.pathname;
+  useEffect(() => {
+    const path = location.pathname;
 
-  if (path.includes('cart')) {
-    setCurretMode("cart");
-  } else if (path.includes('users-info')) {
-    setCurretMode("users-info");
-  } else if (path.includes('change-password')) {
-    setCurretMode("change-password");
-  } else if (path.includes('orders-history')) {
-    setCurretMode("orders-history");
-  } else {
-    setCurretMode("users-info");
-  }
-
-  async function loadUserData() {
-    setIsLoading(true)
-    const API_URL = import.meta.env.VITE_API_URL;
-    const authResult = checkAuthLoader();
-    if (authResult) return authResult;
-    const token = getAuthToken();
-
-    const response = await fetch(API_URL + "/user/user-info", {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json",
-        "Authorization": `Bearer ${token}`
-      }
-    });
-
-    const resData = await response.json();
-    if (response.ok) {
-      setIsLoading(false);
-      return resData;
+    if (path.includes('cart')) {
+      setCurretMode("cart");
+    } else if (path.includes('users-info')) {
+      setCurretMode("users-info");
+    } else if (path.includes('change-password')) {
+      setCurretMode("change-password");
+    } else if (path.includes('orders-history')) {
+      setCurretMode("orders-history");
     } else {
-      setErrorMessage((resData.detail || JSON.stringify(resData)) + ". Please, try to re-login");
-      setIsError(true);
-      removeToken();
-      revalidator.revalidate();
+      setCurretMode("users-info");
     }
-    setIsLoading(false);
-  }
+  }, [location.pathname]);
 
-  loadUserData().then((data) => {
-    setUserInfo(data);
-  });
-}, []);
+  useEffect(() => {
+    async function loadUserData() {
+      setIsLoading(true);
+      const API_URL = import.meta.env.VITE_API_URL;
+      const authResult = checkAuthLoader();
+      if (authResult) return authResult;
+      const token = getAuthToken();
 
+      const response = await fetch(API_URL + "/user/user-info", {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+          "Authorization": `Bearer ${token}`
+        }
+      });
 
-    function setModeToCart(){
-        setCurretMode("cart")
-        navigate('cart');
-    }
-
-    function setModeToEditProfile(){
-        setCurretMode("users-info")
-        navigate('users-info');
-    }
-
-    function setModeToChangePassword(){
-        setCurretMode("change-password")
-        navigate('change-password');
-    }
-
-    function setModeToMyOrders(){
-        setCurretMode("orders-history")
-        navigate('orders-history');
-    }
-
-    function logoutHandler(){
+      const resData = await response.json();
+      if (response.ok) {
+        setUserInfo(resData);
+      } else {
+        setErrorMessage((resData.detail || JSON.stringify(resData)) + ". Please, try to re-login");
+        setIsError(true);
         removeToken();
         revalidator.revalidate();
-        navigate('/');
+      }
+      setIsLoading(false);
     }
 
+    loadUserData();
+  }, []);
 
 
-    return<>
-        <div className={`flex justify-center items-center max-w-[1200px] mx-auto w-full ${isLoading ? 'h-[70vh]' : ''} `}>
-        {isError && <CrititcalErrorWindow message={errorMessage}/>}
-        {isLoading ? <LoadingAnimation className="flex justify-center items-center"/> :
-            <div className='flex max-w-[1200px] w-full mx-auto mt-4 gap-6'>
-                <div className="flex flex-col bg-white shadow-md rounded-lg w-[300px] h-[330px] gap-4 p-6">
-                <h2 className="text-gray-800 text-3xl font-semibold text-center">
-                    Hello {userInfo ? userInfo.first_name : null}
-                </h2>
-                <ProfileMenu
-                    onBasketClick={setModeToCart}
-                    onEditProfileClick={setModeToEditProfile}
-                    onChangePasswordClick={setModeToChangePassword}
-                    onMyOrdersClick={setModeToMyOrders}
-                    logoutHandler={logoutHandler}
-                />
-                </div>
-                {currentMode === "сart" ? <CartComponent/>: 
-                currentMode === "users-info" ? <UserInfo/>:
-                currentMode === "change-password" ? <ChangePassword/>:
-                currentMode === "orders-history" ? <OrdersHistory/>:
-                currentMode === "cart" ? <CartComponent/>:
-                null}
-            </div>}
-        </div>
-    </>
+  function setModeToCart(){
+      setCurretMode("cart")
+      navigate('cart');
+  }
+
+  function setModeToEditProfile(){
+      setCurretMode("users-info")
+      navigate('users-info');
+  }
+
+  function setModeToChangePassword(){
+      setCurretMode("change-password")
+      navigate('change-password');
+  }
+
+  function setModeToMyOrders(){
+      setCurretMode("orders-history")
+      navigate('orders-history');
+  }
+
+  function logoutHandler(){
+      removeToken();
+      revalidator.revalidate();
+      navigate('/');
+  }
+
+
+
+  return<>
+      <div className={`flex justify-center items-center max-w-[1200px] mx-auto w-full ${isLoading ? 'h-[70vh]' : ''} `}>
+      {isError && <CrititcalErrorWindow message={errorMessage}/>}
+      {isLoading ? <LoadingAnimation className="flex justify-center items-center"/> :
+          <div className='flex max-w-[1200px] w-full mx-auto mt-4 gap-6'>
+              <div className="flex flex-col bg-white shadow-md rounded-lg w-[300px] h-[330px] gap-4 p-6">
+              <h2 className="text-gray-800 text-3xl font-semibold text-center">
+                  Hello {userInfo ? userInfo.first_name : null}
+              </h2>
+              <ProfileMenu
+                  onBasketClick={setModeToCart}
+                  onEditProfileClick={setModeToEditProfile}
+                  onChangePasswordClick={setModeToChangePassword}
+                  onMyOrdersClick={setModeToMyOrders}
+                  logoutHandler={logoutHandler}
+              />
+              </div>
+              {currentMode === "cart" ? <CartComponent/>: 
+              currentMode === "users-info" ? <UserInfo/>:
+              currentMode === "change-password" ? <ChangePassword/>:
+              currentMode === "orders-history" ? <OrdersHistory/>:
+              currentMode === "cart" ? <CartComponent/>:
+              null}
+          </div>}
+      </div>
+  </>
 }
 
 export default Profile;
-
-//Need to add JWT ERROR
-export async function loader() {
-    // const API_URL = import.meta.env.VITE_API_URL;
-    // const authResult = checkAuthLoader();
-    // if (authResult) return authResult;
-    // const token = getAuthToken();
-
-    // const response = await fetch(API_URL + "/user/user-info", {
-    //     method: "GET",
-    //     headers: {
-    //     "Content-type":"application/json",
-    //     "Authorization": `Bearer ${token}`
-    //     }
-    // });
-
-    // const resData = await response.json();
-    // return resData;
-}
