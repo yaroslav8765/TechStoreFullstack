4a07376bdee4404b50bc5aa11b2a44f2e4d25a14
yaroslav8765 <pechorkin2014@gmail.com>
2025-07-01 10:51:07 +0300

    feat: implement OrderCongratulations component to display order confirmation and navigate back to home

--- frontend/src/routers/OrderCongratulations.jsx
+++ frontend/src/routers/OrderCongratulations.jsx
@@ -1,10 +1,25 @@
+import { useParams, useNavigate } from "react-router-dom";
 
+function OrderCongratulations() {
+  const { order_number } = useParams();
+  const navigate = useNavigate();
 
-function OrderCongratulations(){
-
-    return <div>
-        <p className="text-gray-900">dafkjh;akfdjhjk;j</p>
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold mb-4 text-gray-700">
+        Thank you for your order!
+      </h1>
+      <p className="text-lg text-gray-800 mb-6">
+        Your order number is <span className="font-semibold">{order_number}</span>.
+      </p>
+      <button
+        onClick={() => navigate("/")}
+        className="px-6 py-3 gradient-btn-green text-white rounded-lg "
+      >
+        Back to Home
+      </button>
     </div>
+  );
 }
 
 export default OrderCongratulations;
